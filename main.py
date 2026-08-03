2eaaf1d0f650b743323da8461369ed72bf70a7f2
Donald <d@gmail.com>
2026-08-03 21:21:11 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import serverless_wsgi  # Import the AWS Lambda adapter wrapper
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Welcome to my Serverless Flask App!</h1>"
+
+@app.get('/api/health')
+def health_check():
+    return jsonify({"status": "healthy"}), 200
+
+# This handler function is what AWS Lambda calls whenever an HTTP request arrives
+def handler(event, context):
+    return serverless_wsgi.handle_request(app, event, context)
